ed91331b32465010be363f9b3096d3d5fb6756de
eltigre1752 <56106166+eltigre1752@users.noreply.github.com>
2019-11-21 22:54:37 +0700

    Add files via upload

---
+++ Sourcecode chương 8/CustomButton.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CustomRendererExample 
+{ 
+    public partial class CustomButton : Button 
+    { 
+        public CustomButton() 
+        { 
+            InitializeComponent(); 
+        } 
+    } 
+}
---
+++ Sourcecode chương 8/EffectExamplePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectMVVM_FlowerOnline._2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EffectExamplePage : ContentPage
+    {
+        public EffectExamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
